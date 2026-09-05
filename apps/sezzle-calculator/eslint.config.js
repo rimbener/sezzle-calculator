80b301c2786891b36dc08e420afa8184c13c5cfe
Hernán Laura <hernanlaura@gmail.com>
2026-09-05 10:01:58 -0300

    added empty sezzle-calculator app

--- apps/sezzle-calculator/eslint.config.js
+++ apps/sezzle-calculator/eslint.config.js
@@ -1,9 +1,10 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import stylistic from '@stylistic/eslint-plugin'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -14,6 +15,7 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      stylistic.configs.recommended,
     ],
     languageOptions: {
       globals: globals.browser,
